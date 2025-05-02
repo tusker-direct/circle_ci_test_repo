f76daedc1a91ef12796eab2ada57d10cc7b1d487
Gabriel Zapata <gabrielz@TUSL-MAC22.local>
2025-05-02 11:26:44 +0100

    add some testing

--- my_app/lib/MyApp.pm
+++ my_app/lib/MyApp.pm
@@ -3,18 +3,17 @@ use Mojo::Base 'Mojolicious', -signatures;
 
 # This method will run once at server start
 sub startup ($self) {
-
   # Load configuration from config file
-  my $config = $self->plugin('NotYAMLConfig');
+my $config = $self->plugin('NotYAMLConfig');
 
-  # Configure the application
-  $self->secrets($config->{secrets});
+# Configure the application
+$self->secrets($config->{secrets});
 
-  # Router
-  my $r = $self->routes;
+# Router
+my $r = $self->routes;
 
-  # Normal route to controller
-  $r->get('/')->to('Example#welcome');
+# Normal route to controller
+$r->get('/')->to('Example#welcome');
 }
 
 1;
