0fb2ba9cd8fcec56aa058265611fce36eac3ca6f
DragosTrif <Dragos.Trif@tuskerdirect.com>
2025-05-09 19:31:09 +0300

    load env vars

--- my_app/utils/db_health_check.sh
+++ my_app/utils/db_health_check.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
+
+# Load env vars manually
+if [ -f /env_vars.env ]; then
+  export $(grep -v '^#' /.env | xargs)
+fi
 mariadb -h127.0.0.1 -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "SELECT 1" "$MYSQL_DATABASE" > /dev/null 2>&1 || exit 1
