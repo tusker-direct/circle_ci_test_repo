3bdf704b80034fe602d2581b609b6d8d942e2226
Gabriel Zapata <gabrielz@TUSL-MAC22.local>
2025-05-13 16:06:24 +0200

    add with CMD command

--- my_app/utils/db_health_check.sh
+++ my_app/utils/db_health_check.sh
@@ -11,4 +11,4 @@ echo "MYSQL_PASSWORD=$MYSQL_PASSWORD"
 echo "MYSQL_DATABASE=$MYSQL_DATABASE"
 
 # Actual health check
-mariadb -h127.0.0.1 -u"myuser" -p"password" -e "SELECT 1" "my_app" > /dev/null 2>&1 || exit 1
+mariadb -h127.0.0.1 -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "SELECT 1" "$MYSQL_DATABASE" > /dev/null 2>&1 || exit 1
